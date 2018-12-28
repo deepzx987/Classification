410c093be7611cc9c19eee1d344fb97440ff1167
deepankar <d.nankani@iitg.ernet.in>
2018-12-28 17:33:01 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,4 @@
+def main(multi_mode='ovo', winL=90, winR=90, do_preprocess=True, use_weight_class=True,
+         maxRR=True, use_RR=True, norm_RR=True, compute_morph={''}, oversamp_method = '',
+         pca_k = '', feature_selection = '', do_cross_val = '', C_value = 0.001, gamma_value = 0.0,
+         reduced_DS = False, leads_flag = [1,0]):
