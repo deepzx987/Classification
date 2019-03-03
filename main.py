e27ec05ed01b76941e02253733a335507e84f20f
deepzx987 <d.nankani@iitg.ernet.in>
2019-03-03 16:46:11 +0530

    Included V lead and Pre RR interval

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data_load import *
 
 def main(multi_mode='ovo', winL=90, winR=90, do_preprocess=True, use_weight_class=True,
          maxRR=True, use_RR=True, norm_RR=True, compute_morph={''}, oversamp_method='',
-         pca_k='',feature_selection='', do_cross_val='', C_value=0.001, gamma_value=0.0,
+         pca_k='', feature_selection='', do_cross_val='', C_value=0.001, gamma_value=0.0,
          reduced_DS=False, leads_flag=[1, 0]):
     """
 
